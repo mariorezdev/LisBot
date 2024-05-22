9674636960cca1349eeffee1e585a4a93087dbce
Mario Rezende <mariorez@gmail.com>
2024-05-22 15:19:03 -0300

    test: add scenario for CommandManager

--- src/test/java/dev/seariver/command/CommandManagerTest.java
+++ src/test/java/dev/seariver/command/CommandManagerTest.java
@@ -1,13 +1,16 @@
 package dev.seariver.command;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CommandManagerTest {
 
     @Test
-    void GIVEN_valida_command_WHEN_find_command_MUST_return_command_instance() {
+    void GIVEN_valid_command_WHEN_find_command_MUST_return_present() {
 
         // GIVEN
         var textCommand = "/l";
@@ -18,5 +21,25 @@ class CommandManagerTest {
 
         // THEN
         assertThat(optionalCommand).isPresent();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {
+        "  ",
+        "l",
+        "//l",
+        "/x",
+    })
+    void GIVEN_invalid_or_nonexistent_command_WHEN_find_command_MUST_return_empty(String textCommand) {
+
+        // GIVEN
+        CommandManager.instance().addCommands(new ListCommand());
+
+        // WHEN
+        var optionalCommand = CommandManager.instance().findCommand(textCommand);
+
+        // THEN
+        assertThat(optionalCommand).isEmpty();
     }
 }
